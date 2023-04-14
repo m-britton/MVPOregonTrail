e39ba52b4693a63930c97d40c92ef6993ee82625
m-britton <112112547+m-britton@users.noreply.github.com>
2023-04-14 07:48:59 -0400

    Add files via upload

--- RandomEvents.java
+++ RandomEvents.java
@@ -8,16 +8,34 @@
  *              own likelihood, and it is the same for each day traveling along the trail.
  */
 
-package com.example.mvpfororegontrail;
+package com.example.mp2oregontrailmvp;
+import androidx.annotation.Nullable;
 
 import java.util.Random;
 
 public class RandomEvents {
+
     // Constant variables that show the likelihood of an event occurring
     private final double snakeBite = 0.7;
     private final double indiansFindFood = 5.0;
     private final double wrongWay = 1.0;
-
+    private final double lostTrail = 2.0;
+    private final double roughTrail = 2.5;
+    private final double impossibleTrail = 2.5;
+    private final double foundWildFruit = 4.0;
+    private final double fire = 2.0;
+    private final double lostMember = 1.0;
+    private final double missingOx = 1.0;
+    private final double abandonedWagon = 2.0;
+    private final double blizzard = 15.0;
+    private final double foggy = 6.0;
+    private final double hail = 6.0;
+    private final double prairieInjury = 2.0;
+    private final double mountainInjury = 3.5;
+    private final double thief = 2.0;
+    private final double badWater = 10.0;
+    private final double veryLittleWater = 20.0;
+    private final double inadequateGrass = 20.0;
     // The probability of an event happening
     public double eventProbability = 0.0;
 
@@ -78,4 +96,106 @@ public class RandomEvents {
         return eventProbability <= wrongWay;
     }
 
+//    public boolean severeStorm(){
+//
+//    }
+
+    public boolean severeBlizzard(){
+        setEventProbability();
+        return eventProbability <= blizzard;
+    }
+
+    public boolean heavyFog(){
+        setEventProbability();
+        return eventProbability <= foggy;
+    }
+
+    public boolean hailStorm(){
+        setEventProbability();
+        return eventProbability <= hail;
+    }
+
+    public boolean injuredOxenPrairie(){
+        setEventProbability();
+        return eventProbability <= prairieInjury;
+    }
+
+    public boolean injuredOxenMountain(){
+        setEventProbability();
+        return eventProbability <= mountainInjury;
+    }
+
+    public boolean injuredPersonPrairie(){
+        setEventProbability();
+        return eventProbability <= prairieInjury;
+    }
+
+    public boolean injuredPersonMountain(){
+        setEventProbability();
+        return eventProbability <= mountainInjury;
+    }
+
+    public boolean lostTrail(){
+        setEventProbability();
+        return eventProbability <= lostTrail;
+    }
+
+    public boolean roughTrail(){
+        setEventProbability();
+        return eventProbability <= roughTrail;
+    }
+
+    public boolean impossibleTrail(){
+        setEventProbability();
+        return eventProbability <= impossibleTrail;
+    }
+
+    public boolean foundFruit(){
+        setEventProbability();
+        return eventProbability <= foundWildFruit;
+    }
+
+    public boolean wagonFire(){
+        setEventProbability();
+        return eventProbability <= fire;
+    }
+
+    public boolean lostPerson(){
+        setEventProbability();
+        return eventProbability <= lostMember;
+    }
+
+    public boolean lostOxen(){
+        setEventProbability();
+        return  eventProbability <= missingOx;
+    }
+
+    public boolean foundWagon(){
+        setEventProbability();
+        return eventProbability <= abandonedWagon;
+    }
+
+    public boolean robbedAtNight(){
+        setEventProbability();
+        return eventProbability <= thief;
+    }
+
+    public boolean badWater(){
+        setEventProbability();
+        return eventProbability <= badWater;
+    }
+
+    public boolean littleWater(){
+        setEventProbability();
+        return eventProbability <= veryLittleWater;
+    }
+
+    public boolean badGrass(){
+        setEventProbability();
+        return eventProbability <= inadequateGrass;
+    }
+
+//    public boolean illness(){
+//        setEventProbability();
+//    }
 }
