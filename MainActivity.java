a836c6961ae13a38526a51a342d1eae7f7324b7e
m-britton <112112547+m-britton@users.noreply.github.com>
2023-04-25 14:38:29 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -158,7 +158,9 @@ public class MainActivity extends AppCompatActivity {
                 location.setDistanceToTown(location.getDistanceToTown() - 20);
 
                 location.atLandmark();
-
+                
+                // display the weather for the day to the user, using the getTemperature method
+                weatherBox.setText(weather.displayTemperature(weather.getTemperature(1, location.getMonth())));
 
                 //Displays a message if the person is at a town. If true resets distance to next landmark.
                 if (location.getAtTown()) {
@@ -182,47 +184,92 @@ public class MainActivity extends AppCompatActivity {
                 // restore 10 percent of the total health for resting over night
                 overallHealth.startOfDayHealth();
 
+                // Reset the amount of accumulated precipitation.
+                weather.resetRain();
+                weather.resetSnow();
+
                 /* Find whether or not a random event occurs each day.
-                    If someone gets bit by a snake the overall health goes up by 10.
-                    If Indians help you find food the amount of food goes up 8 pounds.
-                    If you take the wrong trail you have to back track using vital supplies.
-                    If a random event takes place a prompt appears for the user to see what happened.
+                    If a random event takes place a prompt appears for the user to see what happened
+                    If someone gets bit by a snake the overall health goes up by 10
+                    If Indians help you find food the amount of food goes up 8 pounds
+                    If you take the wrong trail you have to back track using vital supplies
+                    Otherwise they message "It was a normal day" apears
                  */
                 if (re.snakeBite()) {
-                    overallHealth.addHealth(10);
+                    health.addHealth(10);
                     reBox.setText("You got bit by a snake");
-                } else if (re.indiansHelpFindFood()) {
-//                    Wagon.setFood(Wagon.getFood() + 8);
+                    health.sickPerson(health.randomPerson());
+                }
+                else if (re.indiansHelpFindFood()) {
+                   wagon.setFood(wagon.getFood() + 30);
                     reBox.setText("Indians helped you find food");
-                } else if (re.wrongTrail()) {
-
+                }
+                else if (re.wrongTrail()) {
                     reBox.setText("OH-NO you took the wrong trail");
-                } else {
+                }
+                else if(re.lostTrail()){
+                    reBox.setText("You lost the trail");
+                }
+                else if(re.wagonFire()){
+                    reBox.setText("THe wagon caught on fire!!");
+                }
+                else if(re.lostPerson()){
+                    reBox.setText("A member of your group is missing");
+                }
+                else if(re.lostOxen()){
+                    reBox.setText("An oxen has wandered off.");
+                }
+                else if(re.foundWagon()){
+                    reBox.setText("You found an abandoned wagon");
+                }
+                else if(re.robbedAtNight()){
+                    reBox.setText("You got robbed in the niddle of the night");
+                }
+                else {
                     reBox.setText("It was a normal day!");
                 }
+                if(weather.getTotalRain() <= 0.1){
+                    if(re.badWater()){
+                        reBox.setText("The water is bad.");
+                        health.addHealth(20);
+                    }
+                    else if(re.littleWater()){
+                        reBox.setText("There is very little water");
+                        health.addHealth(10);
+                    }
+                    else if(re.badGrass()) {
+                        reBox.setText("The grass is inadequate.");
+                        health.addOxenHealth(15);
+                    }
+                }
+                if(location.getMonth() >= 5 && location.getMonth() <= 9){
+                    if(re.foundFruit()){
+                        reBox.setText("You found wild fruit");
+                        wagon.setFood(wagon.getFood() + 20);
+                    }
+                }
 
-                //Depending on how many pounds of food is remaining the ration qualification changes.
+                //Depending on how many pounds of food is remaining the ration qualification changes
                 if (wagon.getFood() >= 1500) {
                     rations = "Filling";
                 } else if (wagon.getFood() >= 1000) {
                     rations = "Meager";
-                } else if (wagon.getFood() >= 5000) {
+                } else if (wagon.getFood() >= 500) {
                     rations = "Bare Bones";
                 } else {
                     rations = "Out of food";
                 }
 
-                // Using the rations found above determine how the health is effected.
-                overallHealth.healthFromFood(rations);
+                // Using the rations found above determine how the health is effected
+                health.healthFromFood(rations);
 
                 wagon.setFood(wagon.getFood() - 20);
 
                 // display to the user the overall health of the entire group
-                healthBox.setText(overallHealth.generalHealth());
+                healthBox.setText(health.generalHealth());
                 foodBox.setText(Double.toString(wagon.getFood()));
                 date.setText(location.whatMonth() + " " + location.getDay());
             }
         });
 
     }
-}
